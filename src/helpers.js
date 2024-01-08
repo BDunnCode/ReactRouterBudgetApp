bad17f3a90cb765db0e454be693610290fc172bc
BDunnCode <BDunnCode@github.com>
2024-01-07 20:21:02 -0600

    create cards for existing budges in localStorage

--- src/helpers.js
+++ src/helpers.js
@@ -47,9 +47,37 @@ export const createExpense = ({
   JSON.stringify([...existingExpenses, newItem]))
 }
 
-
-
 // delete item
 export const deleteItem = ({key}) => {
   return localStorage.removeItem(key)
+}
+
+// total spent by budget
+export const calculateSpentByBudget = (budgetId) => {
+  const expenses = fetchData("expenses") ?? []
+  const budgetSpent = expenses.reduce((acc, expense) => {
+    // check if the expense.id === budgetId passed in
+    if(expense.budgetId !== budgetId) return acc
+
+    // add the current amount to my total
+    return acc += expense.amount 
+  }, 0)
+  return budgetSpent
+}
+
+
+// Formatting
+export const formatPercentage = (amt) => {
+  return amt.toLocaleString(undefined, {
+    style: "percent",
+    minimumFractionDigits: 0,
+  })
+}
+
+// Format currency
+export const formatCurrency = (amt) => {
+  return amt.toLocaleString(undefined, {
+    style: "currency",
+    currency: "USD"
+  })
 }
