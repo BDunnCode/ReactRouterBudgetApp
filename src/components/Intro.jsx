bd67ad99cf32140d9b4a6defed7b045acd41f3ec
BDunnCode <BDunnCode@github.com>
2024-01-09 14:57:53 -0600

    used the isSubmitting logic from AddExpenseForm to prevent multiple presses on user creation

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,4 +1,5 @@
-import { Form } from "react-router-dom"
+// react-router-dom imports
+import { useFetcher } from "react-router-dom"
 
 // library
 import { UserPlusIcon } from "@heroicons/react/24/solid"
@@ -6,7 +7,14 @@ import { UserPlusIcon } from "@heroicons/react/24/solid"
 // assets
 import illustration from "../assets/illustration.jpg"
 
+
+
 const Intro = () => {
+  const fetcher = useFetcher()
+  const isSubmitting = fetcher.state === 
+  "submitting"
+
+
   return ( 
     <div className="intro">
       <div>
@@ -17,7 +25,7 @@ const Intro = () => {
           Personal budgeting is the secret to financial freedom. Start your 
           journey today.
         </p>
-        <Form method="post">
+        <fetcher.Form method="post">
           <input 
             type="text" 
             name="userName" 
@@ -27,11 +35,17 @@ const Intro = () => {
             autoComplete="given-name" 
             />
             <input type="hidden" name="_action" value="newUser" />
-            <button type="submit" className="btn btn--dark">
-              <span>Create Account</span>
-              <UserPlusIcon width={20} />
+            <button type="submit" className="btn btn--dark" disabled={isSubmitting}>
+              {  
+                isSubmitting ? <span>Submitting...</span> : (
+                <>
+                  <span>Create Account</span>
+                  <UserPlusIcon width={20} />
+                </>
+                )
+              }
             </button>
-        </Form>
+        </fetcher.Form>
       </div>
       <img src={ illustration } alt="Person with money" 
       width={600} />
