d28a816b84f8f91a24e5445f4a2deb6980c47171
BDunnCode <BDunnCode@github.com>
2024-01-09 21:28:04 -0600

    setting up for final commit

--- src/components/BudgetItem.jsx
+++ src/components/BudgetItem.jsx
@@ -1,8 +1,13 @@
-// helper functions
+// react-router-dom imports
+import { Form, Link } from "react-router-dom"
 
+// library imports
+import { BanknotesIcon } from "@heroicons/react/24/outline"
+
+// helper functions
 import { calculateSpentByBudget, formatCurrency, formatPercentage } from "../helpers"
 
-const BudgetItem = ({budget}) => { 
+const BudgetItem = ({ budget , showDelete = false }) => { 
    const { id, name, amount, color } = budget
    const spent = calculateSpentByBudget(id)
 
@@ -12,11 +17,10 @@ const BudgetItem = ({budget}) => {
       style={{
         "--accent": color
       }}
-    
     >
       <div className="progress-text">
         <h3>{name}</h3>
-        <p>{ formatCurrency(amount)} Budgeted</p>
+        <p>{formatCurrency(amount)} Budgeted</p>
       </div>
       <progress max={amount} value={spent}>
         {formatPercentage(spent / amount)}
@@ -25,6 +29,18 @@ const BudgetItem = ({budget}) => {
         <small>{formatCurrency(spent)} spent</small>
         <small>{formatCurrency(amount - spent)}</small>
       </div>
+      {
+        showDelete ? (
+          <Form>
+            <p>hi</p>
+          </Form>
+        ) : (
+          <Link>
+            <span>View Details</span>
+            <BanknotesIcon width={20} />
+          </Link>
+        ) 
+      }
     </div>
   )
 }
--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -5,9 +5,8 @@ import { Link, useFetcher } from "react-router-dom"
 import { formatCurrency, formatDateToLocaleString, getAllMatchingItems } from "../helpers"
 import { TrashIcon } from "@heroicons/react/24/solid"
 
-const ExpenseItem = ({ expense }) => {
+const ExpenseItem = ({ expense, showBudget }) => {
   const fetcher = useFetcher()
-
   const budget = getAllMatchingItems({
     category: "budgets",
     key: "id",
@@ -19,7 +18,9 @@ const ExpenseItem = ({ expense }) => {
       <td>{expense.name}</td>
       <td>{formatCurrency(expense.amount)}</td>
       <td>{formatDateToLocaleString(expense.createdAt)}</td>
-      <td><Link
+      {showBudget && (
+      <td>
+         <Link
         to={`/budget/${budget.id}`}
         style={{
           "--accent": budget.color,
@@ -28,6 +29,7 @@ const ExpenseItem = ({ expense }) => {
           {budget.name}
         </Link>
       </td>
+      )}
       <td>
         <fetcher.Form method="post">
           <input type="hidden" name="_action"
