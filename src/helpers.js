3b98afed35fdcbc509c375de637749022f043862
BDunnCode <BDunnCode@github.com>
2024-01-09 18:58:05 -0600

    add expenses page delete functionality

--- src/helpers.js
+++ src/helpers.js
@@ -2,18 +2,33 @@ export const wait = () => new Promise(
   res => setTimeout(res, Math.random() * 800)
 )
 
-
-
+//colors
 const generateRandomColor = () => {
   const existingBudgetLength = fetchData("budgets")?.
   length ?? 0;
   return `${existingBudgetLength * 34} 65% 50%`
 }
 
-
 // Local storage
 export const fetchData = (key) => {
   return JSON.parse(localStorage.getItem(key))
+}
+
+// Get all items from localStorage
+export const getAllMatchingItems = ({category, key, value}) => {
+    const data = fetchData(category) ?? []
+    return data.filter((item) => item[key] === value)
+  }
+
+// delete item from localStorage
+export const deleteItem = ({key, id}) => {
+  const existingData = fetchData(key)
+  if(id){
+    const newData = existingData.filter((item) => 
+    item.id !== id)
+    return localStorage.setItem(key, JSON.stringify(newData))
+  }
+  return localStorage.removeItem(key)
 }
 
 // create budget
@@ -45,11 +60,6 @@ export const createExpense = ({
   const existingExpenses = fetchData("expenses") ?? []
   return localStorage.setItem("expenses", 
   JSON.stringify([...existingExpenses, newItem]))
-}
-
-// delete item
-export const deleteItem = ({key}) => {
-  return localStorage.removeItem(key)
 }
 
 // total spent by budget
